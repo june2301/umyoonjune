f695067c5332dbe4ff58ab9d8cc5f1433085c6bd
jjune2301 <jjune2301@gmail.com>
2024-08-14 00:07:26 +0900

    0814스터디문제

--- Algo/src/baekjoon/카드정렬_1715.java
+++ Algo/src/baekjoon/카드정렬_1715.java
@@ -3,46 +3,32 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 
 public class 카드정렬_1715 {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		int N = Integer.parseInt(br.readLine());
-		
-		int[] card = new int[N];
+
+		PriorityQueue<Integer> q = new PriorityQueue<>();
+
 		for (int i = 0; i < N; i++) {
-			card[i] = Integer.parseInt(br.readLine());
+			q.add(Integer.parseInt(br.readLine()));
 		}
-		Arrays.sort(card);
-		Queue<Integer> q = new LinkedList<>();
-		
-		
-		int save = card[0];
-		q.add(save);
+
 		int sum = 0;
-		int i = 1;
-		while (i < N) {
-			save = q.poll();
-			q.add(card[i++]);
-			sum += save + q.poll();
-			q.add(sum);
+		while (q.size() > 1) {
+			int first = q.poll();
+			int second = q.poll();
+			int save = first + second;
+
+			sum += save;
+			q.add(save);
 		}
-		
-//		int sum = 0;
-//		int j = 1;
-//		while (j < N) {
-//			for (int i = 0; i <= j;i++) {
-//				q.add(card[i]);
-//				sum += q.poll();
-//			}
-//			j++;
-//		}
+
 		System.out.println(sum);
 	}
 }
