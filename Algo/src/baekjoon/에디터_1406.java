6b376dd6f215aeed63fbe4d20568f0d22536e9cd
jjune2301 <jjune2301@gmail.com>
2024-08-16 02:35:13 +0900

    에디터 수정but실패

--- Algo/src/baekjoon/에디터_1406.java
+++ Algo/src/baekjoon/에디터_1406.java
@@ -1,60 +1,57 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
 
 public class 에디터_1406 {
 
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 		String input = br.readLine();
-		
-		char[] text = input.toCharArray();
-		List<Character> txt = new ArrayList<Character>();
-		
-		for (int i = 0; i < text.length; i++) {
-			txt.add(text[i]);
-		}
-		
-		int loc = txt.size();
-		
-		int N = Integer.parseInt(br.readLine());
-		for (int i = 0; i < N; i++) {
-			String[] cmd = br.readLine().split(" ");
-			if (cmd[0].equals("L")) {
-				loc--;
-				if (loc < 1) {
-					loc = 0;
-				}
-			} else if (cmd[0].equals("D")) {
-				loc++;
-				if (loc > txt.size() - 1) {
-					loc = txt.size();
-				}
-			} else if (cmd[0].equals("B")) {
-				if (loc == 0) {
-					
-				} else {
-					txt.remove(loc-1);
-					loc--;
-				}
-			} else if (cmd[0].equals("P")) {
-				txt.add(loc, cmd[1].charAt(0));
-				loc++;
-			}
-		}
-		String ans = "";
-		for (int i = 0; i < txt.size(); i++) {
-			ans += txt.get(i);
-		}
-		
-		System.out.println(ans);
-		
-	}
+	        LinkedList<Character> txt = new LinkedList<>();
+
+	        for (char c : input.toCharArray()) {
+	            txt.add(c);
+	        }
+
+	        int loc = txt.size();
+
+	        int N = Integer.parseInt(br.readLine());
+	        for (int i = 0; i < N; i++) {
+	            String[] cmd = br.readLine().split(" ");
+	            if (cmd[0].equals("L")) {
+	                if (loc > 0) {
+	                    loc--;
+	                }
+	            } else if (cmd[0].equals("D")) {
+	                if (loc < txt.size()) {
+	                    loc++;
+	                }
+	            } else if (cmd[0].equals("B")) {
+	                if (loc > 0) {
+	                    txt.remove(loc - 1);
+	                    loc--;
+	                }
+	            } else if (cmd[0].equals("P")) {
+	                txt.add(loc, cmd[1].charAt(0));
+	                loc++;
+	            }
+	        }
+
+	        StringBuilder sb = new StringBuilder();
+	        for (char c : txt) {
+	            sb.append(c);
+	        }
+
+	        bw.write(sb.toString());
+	        bw.flush(); 
+	        bw.close();
+	    }
 
 }
