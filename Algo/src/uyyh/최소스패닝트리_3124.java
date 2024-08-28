723423d294a7aa96cce22c3459562acfdd9e0478
hongtaehwi <paul960801@gmail.com>
2024-08-28 17:57:23 +0900

    0828 17:57

--- Algo/src/uyyh/최소스패닝트리_3124.java
+++ Algo/src/uyyh/최소스패닝트리_3124.java
@@ -1,9 +1,44 @@
 package uyyh;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class 최소스패닝트리_3124 {
 
+	static class Node implements Comparable<Node> {
+		int from;
+		int to;
+		int value;
+		
+		public Node(int from, int to, int value) {
+			this.from = from;
+			this.to = to;
+			this.value = value;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return this.value - o.value;
+		}
+		
+	}
+	
+/*
+
+1
+3 3
+1 2 1
+2 3 2
+1 3 3
+from to value
+
+ */
+	static List<Node> nList;
+	static int ans = 0;
+	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
@@ -11,19 +46,39 @@ public class 최소스패닝트리_3124 {
 		int V = sc.nextInt();
 		int E = sc.nextInt();
 		
-		int[][] node = new int[E][E];
+		nList = new ArrayList<>();
 		for (int i = 0; i < E; i++) {
-			for (int j = 0; j < E; j++) {
-				node[i][j] = sc.nextInt();
-			}
+			int from = sc.nextInt();
+			int to = sc.nextInt();
+			int value = sc.nextInt();
+			Node input = new Node(from, to, value);
+			nList.add(input);
 		}
 		
-		// 노드 파고
-		// 입력 받은거 노드에 넣고
-		// pq에 첫번째 넣고
-		// pq에서 빼고 가중치 비교하면서 더하기
+	}
+	
+	static void prim(int start) {
+		
+		Queue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(1,0,0));
+		
+		while (!pq.isEmpty()) {
+			Node save = pq.poll();
+			int from = save.from;
+			int to = save.to;
+			int value = save.value;
+			
+			// to 들 중에 제일 낮은 value 선택
+			// 
+			
+		}
+		
+		
+		
 		
 		
 	}
+	
+	
 
 }
