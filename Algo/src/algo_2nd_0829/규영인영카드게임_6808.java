b068d42c5b21d52639e106d912bc30cd562439ed
엄윤준 <jjune2301@gmail.com>
2024-09-08 14:07:39 +0900

    노트북에서 오류수정

--- Algo/src/algo_2nd_0829/규영인영카드게임_6808.java
+++ Algo/src/algo_2nd_0829/규영인영카드게임_6808.java
@@ -1,50 +1,20 @@
 package algo_2nd_0829;
 
 import java.util.ArrayList;
-<<<<<<< HEAD
-import java.util.Arrays;
-=======
->>>>>>> 94c9e6f84e12ced90e5eecb53bf63ae5d82a32c3
 import java.util.List;
 import java.util.Scanner;
 
 public class 규영인영카드게임_6808 {
-<<<<<<< HEAD
-=======
 	
 	static int[] nums, kyu, in;
 	static int[] deck;
 	static boolean[] visit;
 	static int kwin, iwin;
->>>>>>> 94c9e6f84e12ced90e5eecb53bf63ae5d82a32c3
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
-<<<<<<< HEAD
-		int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
-		
-		int[] kyu = new int[9];
-		List<Integer> save = new ArrayList<>();
-		int[] in = new int[9];
-		for (int i = 0; i < 9; i++) {
-			kyu[i] = sc.nextInt();
-			nums[kyu[i]-1] = 0;
-		}
-		for (int i = 0; i < nums.length; i++) {
-			if (nums[i] != 0) {
-				save.add(nums[i]);
-			}
-		}
-		for (int i = 0; i < in.length; i++) {
-			in[i] = save.get(i);
-		}
-		
-		System.out.println(Arrays.toString(kyu));
-		System.out.println(Arrays.toString(in));
-		
-=======
 		int T = sc.nextInt();
 		for (int tc = 1; tc <= T; tc++) {
 			nums = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
@@ -107,7 +77,6 @@ public class 규영인영카드게임_6808 {
 			kwin++;
 		}
 		
->>>>>>> 94c9e6f84e12ced90e5eecb53bf63ae5d82a32c3
 	}
 
 }
