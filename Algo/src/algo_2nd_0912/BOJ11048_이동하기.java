b627cc0dd0c09a13d08a61b6cb92ff0ef94cb0f6
jjune2301 <jjune2301@gmail.com>
2024-09-12 17:58:45 +0900

    0912 DP

---
+++ Algo/src/algo_2nd_0912/BOJ11048_이동하기.java
@@ -0,0 +1,31 @@
+package algo_2nd_0912;
+
+import java.util.Scanner;
+
+public class BOJ11048_이동하기 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt()+1;
+		int M = sc.nextInt()+1;
+		int[][] map = new int[N][M];
+		for (int r = 1; r < N; r++) {
+			for (int c = 1; c < M; c++) {
+				map[r][c] = sc.nextInt();
+			}
+		}
+		
+		int[][] dp = new int[N][M];
+		for (int r = 1; r < N; r++) {
+			for (int c = 1; c < M; c++) {
+				dp[r][c] = map[r][c] + Math.max(Math.max(dp[r-1][c], dp[r][c-1]), dp[r-1][c-1]);
+			}
+		}
+		
+		System.out.println(dp[N-1][M-1]);
+		
+	}
+
+}
---
+++ Algo/src/algo_2nd_0912/BOJ1149_RGB거리.java
@@ -0,0 +1,36 @@
+package algo_2nd_0912;
+
+import java.util.Scanner;
+
+public class BOJ1149_RGB거리 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int[][] map = new int[N][3];
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < 3; c++) {
+				map[r][c] = sc.nextInt();
+			}
+		}
+		
+		int[][] dp = new int[N][3];
+		
+		for (int c = 0; c < 3; c++) {
+			dp[0][c] = map[0][c];
+		}
+		
+		for (int r = 1; r < N; r++) {
+			dp[r][0] = Math.min(dp[r-1][1] + map[r][0], dp[r-1][2] + map[r][0]);
+			dp[r][1] = Math.min(dp[r-1][0] + map[r][1], dp[r-1][2] + map[r][1]);
+			dp[r][2] = Math.min(dp[r-1][1] + map[r][2], dp[r-1][0] + map[r][2]);
+		}
+		
+		int ans = Math.min(Math.min(dp[N-1][0], dp[N-1][1]), dp[N-1][2]);
+		System.out.println(ans);
+	}
+
+}
