e714e0a6a153b48a8291e4d3fc968bd9e55cb094
jjune2301 <jjune2301@gmail.com>
2024-10-07 00:25:49 +0900

    1006 save

--- Algo/src/uyyh_BOJ/쉬운최단거리BOJ_14940.java
+++ Algo/src/uyyh_BOJ/쉬운최단거리BOJ_14940.java
@@ -1,4 +1,4 @@
-package uyyh;
+package uyyh_BOJ;
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -26,7 +26,7 @@ public class 쉬운최단거리BOJ_14940 {
 		for (int r = 0; r < N; r++) {
 			for (int c = 0; c < M; c++) {
 				map[r][c] = sc.nextInt();
-				if (map[r][c] == 2) {
+				if (map[r][c] == 2) { // 입력받으면서 시작위치 저장 후 0으로 변경
 					str = r;
 					stc = c;
 					map[str][stc] = 0;
@@ -36,9 +36,10 @@ public class 쉬운최단거리BOJ_14940 {
 		
 		cnt = 0;
 		bfs();
+		// bfs 돌리고 바로 출력
 		for (int r = 0; r < N; r++) {
 			for (int c = 0; c < M; c++) {
-				if (!visited[r][c] && map[r][c] == 1) {
+				if (!visited[r][c] && map[r][c] == 1) { // 못가는 지역 -1처리
 					map[r][c] = -1;
 				}
 				System.out.print(map[r][c] + " ");
@@ -47,7 +48,7 @@ public class 쉬운최단거리BOJ_14940 {
 		}
 	}
 	
-	static void bfs() {
+	static void bfs() { // 시작 위치부터 가까운 순으로 1부터 숫자 저장
 		visited = new boolean[N][M];
 		Queue<int[]> q = new LinkedList<>();
 		q.add(new int[] {str, stc});
@@ -55,6 +56,8 @@ public class 쉬운최단거리BOJ_14940 {
 		while (!q.isEmpty()) {
 			int qsize = q.size();
 			cnt++;
+			// qsize 저장 후 cnt 1개씩만 높이면서
+			// 해당 q 안에 있는 애들 같은 숫자 들어가도록.
 			for (int qs = 0; qs < qsize; qs++) {
 				int[] save = q.poll();
 				visited[save[0]][save[1]] = true;
