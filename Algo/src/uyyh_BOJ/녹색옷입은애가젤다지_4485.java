e2ca5719bc66e4ddb79bfbce9197181d8076f55b
jjune2301 <jjune2301@gmail.com>
2024-10-15 15:39:05 +0900

    젤다 푸는중

---
+++ Algo/src/uyyh_BOJ/녹색옷입은애가젤다지_4485.java
@@ -0,0 +1,25 @@
+package uyyh_BOJ;
+
+import java.util.Scanner;
+
+public class 녹색옷입은애가젤다지_4485 {
+	
+	static int[] dr = {-1, 0};
+	static int[] dc = {0, -1};
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[][] map = new int[N][N];
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < N; c++) {
+				map[r][c] = sc.nextInt();
+			}
+		}
+		
+		
+	}
+
+}
