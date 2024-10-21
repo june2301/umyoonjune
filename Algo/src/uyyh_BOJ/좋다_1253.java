8e5e3d21ca25c6ad67b8178818ffbe35e2bf4913
jjune2301 <jjune2301@gmail.com>
2024-10-21 18:01:52 +0900

    푸는중

---
+++ Algo/src/uyyh_BOJ/좋다_1253.java
@@ -0,0 +1,19 @@
+package uyyh_BOJ;
+
+import java.util.Scanner;
+
+public class 좋다_1253 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Algo/src/uyyh_BOJ/회전초밥_1522.java
@@ -0,0 +1,38 @@
+package uyyh_BOJ;
+
+import java.util.Scanner;
+
+public class 회전초밥_1522 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt(); // 접시 수
+		int d = sc.nextInt(); // 초밥 가짓수
+		int k = sc.nextInt(); // 연속 접시 수
+		int c = sc.nextInt(); // 쿠폰 접시
+		
+		int[] dish = new int[N];
+		for (int i = 0; i < N; i++) {
+			dish[i] = sc.nextInt();
+		}
+		
+		int l = 0, r = 0;
+		
+		while (l < N) {
+			
+			if (dish[r] != c) {
+				
+			}
+			
+			
+		}
+		
+		
+		
+		
+		
+	}
+
+}
