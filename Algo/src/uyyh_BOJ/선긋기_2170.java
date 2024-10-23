e05e2239d466c18cd5f0f682fc84011457ac194c
jjune2301 <jjune2301@gmail.com>
2024-10-23 17:59:09 +0900

    문자열교환 클리어, 선긋기 푸는중

---
+++ Algo/src/uyyh_BOJ/선긋기_2170.java
@@ -0,0 +1,17 @@
+package uyyh_BOJ;
+
+import java.util.Scanner;
+
+public class 선긋기_2170 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		
+		
+	}
+
+}
