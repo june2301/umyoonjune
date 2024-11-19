6197a0a151e652ceea6e4ecc3713df54f1017d3d
jjune2301 <jjune2301@gmail.com>
2024-11-20 08:59:25 +0900

    고층건물 푸는중

---
+++ Algo/src/uyyh_BOJ/고층건물_1027.java
@@ -0,0 +1,40 @@
+package uyyh_BOJ;
+
+import java.util.Scanner;
+
+public class 고층건물_1027 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int[] arr = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		// l, r 보내면서 기울기 계산 -> 볼 수 있으면 계속 진행
+		for (int i = 0; i < N; i++) {
+			int cur = arr[i];
+			boolean left = false, right = false;
+			
+			while (!left || !right) {
+				int l = 0;
+				int r = 0;
+				
+				
+				
+				
+			}
+			
+			
+			
+		}
+		
+		
+		
+	}
+
+}
