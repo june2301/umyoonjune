a6ed87a0a0c06876b4fabfc2e413c53415aa950b
jjune2301 <jjune2301@gmail.com>
2024-11-21 09:02:23 +0900

    고층건물 해결

--- Algo/src/uyyh_BOJ/고층건물_1027.java
+++ Algo/src/uyyh_BOJ/고층건물_1027.java
@@ -5,35 +5,49 @@ import java.util.Scanner;
 public class 고층건물_1027 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		int N = sc.nextInt();
-		
+
 		double[] bui = new double[N];
-		
+
 		for (int i = 0; i < N; i++) {
 			bui[i] = sc.nextDouble();
 		}
-		
+
 		int max = 0;
 		for (int i = 0; i < N; i++) {
 			int cnt = 0;
-			
-			double left = Double.NEGATIVE_INFINITY;
+
 			for (int j = i - 1; j >= 0; j--) {
-				double slope = (bui[j]-bui[i]) / (j-i);
-				if (slope > left) {
-					left = slope;
+				boolean check = true;
+				double slope = (bui[j] - bui[i]) / (j - i);
+				
+				for (int k = j + 1; k < i; k++) {
+					double height = bui[i] + slope * (k - i);
+					if (bui[k] >= height) {
+						check = false;
+						break;
+					}
+				}
+				if (check) {
 					cnt++;
 				}
 			}
-			
-			double right = Double.NEGATIVE_INFINITY;
+
 			for (int j = i + 1; j < N; j++) {
-				double slope = (bui[i]-bui[j]) / (i-j);
-				if (slope > right) {
-					right = slope;
+				boolean check = true;
+				double slope = (bui[j] - bui[i]) / (j - i);
+
+				for (int k = i + 1; k < j; k++) {
+					double height = bui[i] + slope * (k - i);
+					if (bui[k] >= height) {
+						check = false;
+						break;
+					}
+				}
+				if (check) {
 					cnt++;
 				}
 			}
