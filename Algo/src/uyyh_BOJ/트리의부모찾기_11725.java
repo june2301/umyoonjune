9b75bde1e07dbc024cfe2544b8719034c3bf3f7f
jjune2301 <jjune2301@gmail.com>
2025-01-20 20:43:30 +0900

    0120 2 clear

--- Algo/src/uyyh_BOJ/트리의부모찾기_11725.java
+++ Algo/src/uyyh_BOJ/트리의부모찾기_11725.java
@@ -3,28 +3,38 @@ package uyyh_BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class 트리의부모찾기_11725 {
 	
+	static int N;
 	static int[] p;
+	static ArrayList<Integer>[] list;
+	static boolean[] visited;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
 		p = new int[N+1];
-		for (int i = 1; i <= N; i++) {
-			p[i] = i;
+		list = new ArrayList[N+1];
+		visited = new boolean[N+1];
+		
+		for (int i = 0; i < N+1; i++) {
+			list[i] = new ArrayList<Integer>();
 		}
-		System.out.println(Arrays.toString(p));
+		
 		for (int i = 0; i < N-1; i++) {
 			String[] ab = br.readLine().split(" ");
 			int a = Integer.parseInt(ab[0]);
 			int b = Integer.parseInt(ab[1]);
-			union(a, b);
-			System.out.println(i + " : " + Arrays.toString(p));
+			
+			list[a].add(b);
+			list[b].add(a);
 		}
+		
+		dfs(1);
 		
 		for (int i = 2; i <= N; i++) {
 			System.out.println(p[i]);
@@ -32,19 +42,15 @@ public class 트리의부모찾기_11725 {
 		
 	}
 	
-	static int find(int x) {
-		if(x != p[x]) {
-			p[x] = find(p[x]);
+	static void dfs(int idx) {
+		visited[idx] = true;
+		for (int num : list[idx]) {
+			if (!visited[num]) {
+				p[num] = idx;
+				dfs(num);
+			}
 		}
-		return p[x];
 	}
 	
-	static void union(int x, int y) {
-		int rx = find(x);
-		int ry = find(y);
-		if (rx != ry) {
-			p[ry] = rx;
-		}
-	}
 
 }
