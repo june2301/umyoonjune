b6edd8990a2082f98082cc9e8731e313bd2ffdf6
jjune2301 <jjune2301@gmail.com>
2025-01-31 09:13:23 +0900

    0131 3 clear 4 save

---
+++ Algo/src/uyyh_BOJ/스티커_9465.java
@@ -0,0 +1,39 @@
+package uyyh_BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 스티커_9465 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			
+			int N = Integer.parseInt(br.readLine());
+			
+			int[][] arr = new int[2][N+1];
+			
+			String[] input1 = br.readLine().split(" ");
+			String[] input2 = br.readLine().split(" ");
+			for (int i = 1; i <= N; i++) {
+				arr[0][i] = Integer.parseInt(input1[i-1]);
+				arr[1][i] = Integer.parseInt(input2[i-1]);
+			}
+			
+			int[][] dp = new int[2][N+1];
+			dp[0][1] = arr[0][1];
+			dp[1][1] = arr[1][1];
+			for (int i = 2; i <= N; i++) {
+				dp[0][i] = Math.max(dp[1][i-1], dp[1][i-2]) + arr[0][i];
+				dp[1][i] = Math.max(dp[0][i-1], dp[0][i-2]) + arr[1][i];
+			}
+			System.out.println(Math.max(dp[0][N], dp[1][N]));
+		}
+		
+	}
+
+}
---
+++ Algo/src/uyyh_BOJ/웜홀_1865.java
@@ -0,0 +1,31 @@
+package uyyh_BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 웜홀_1865 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			
+			// 도로와 웜홀 각각 받아두고
+			// 웜홀 먼저 연결되는지 확인 / 안되면 NO
+			// 웜홀 가능한 경우 시간 저장
+			// 도로로 진행하여 시간보다 적으면 YES
+			
+			
+			
+			
+			
+			
+			
+		}
+		
+	}
+
+}
