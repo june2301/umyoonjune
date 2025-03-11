abddcacda935c1b610f976ef6f20b8f0d0b6c5b5
jjune2301 <jjune2301@naver.com>
2025-03-11 17:57:59 +0900

    save

--- Algo/src/uyyh_BOJ_2/접두사찾기_14426.java
+++ Algo/src/uyyh_BOJ_2/접두사찾기_14426.java
@@ -5,7 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 
+class Trie {
+	HashMap<Character, Trie> children;
+	boolean isEnd;
+
+	Trie() {
+		children = new HashMap<>();
+		isEnd = false;
+	}
+}
+
 public class 접두사찾기_14426 {
+
+	static Trie root;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,25 +26,41 @@ public class 접두사찾기_14426 {
 		int N = Integer.parseInt(NM[0]);
 		int M = Integer.parseInt(NM[1]);
 
-		HashMap<String, Integer> map = new HashMap<>();
+		root = new Trie();
 		for (int i = 0; i < N; i++) {
-			map.put(br.readLine(), i);
+			String word = br.readLine();
+			insert(word);
 		}
 
-		int ans = 0;
+		int count = 0;
 		for (int i = 0; i < M; i++) {
-			String input = br.readLine();
-			for (String key : map.keySet()) {
-				boolean check = key.startsWith(input);
-				if (check) {
-					ans++;
-					break;
-				}
+			String query = br.readLine();
+			if (search(query)) {
+				count++;
 			}
 		}
 
-		System.out.println(ans);
+		System.out.println(count);
+	}
 
+	static void insert(String word) {
+		Trie curr = root;
+		for (char c : word.toCharArray()) {
+			curr.children.putIfAbsent(c, new Trie());
+			curr = curr.children.get(c);
+		}
+		curr.isEnd = true;
+	}
+
+	static boolean search(String prefix) {
+		Trie curr = root;
+		for (char c : prefix.toCharArray()) {
+			if (!curr.children.containsKey(c)) {
+				return false;
+			}
+			curr = curr.children.get(c);
+		}
+		return true;
 	}
 
 }
